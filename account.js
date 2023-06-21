0bc310697ce35914909874ea0af03d85a4bbebfe
hanguyen <lotusviet.uk@gmail.com>
2023-06-21 07:26:39 +0100

    ESLint pass with no warnings with account.js

--- account.js
+++ account.js
@@ -1,49 +1,46 @@
 class Account {
-    constructor (balance) {
-        this.balance = 0;
-        this.transaction = [];
+  constructor (balance) {
+    this.balance = 0
+    this.transaction = []
+  };
 
+  deposit (amount) {
+    this.balance += amount
+    const depositTransaction = {
+      date: (new Date()).toLocaleDateString('en-GB'),
+      credit: amount,
+      debit: '',
+      balance: this.balance
     }
+    this.transaction.push(depositTransaction)
+  }
 
-    deposit(amount){
-        this.balance += amount;
-        const depositTransaction = {
-            date: (new Date()).toLocaleDateString('en-GB'),
-            credit: amount,
-            debit: '',
-            balance: this.balance
-          };
-          this.transaction.push(depositTransaction);
-        
+  withdrawal (amount) {
+    this.balance -= amount
+    const withdrawalTransaction = {
+      date: (new Date()).toLocaleDateString('en-GB'),
+      credit: '',
+      debit: amount,
+      balance: this.balance
     }
+    this.transaction.push(withdrawalTransaction)
+  }
 
-    withdrawal(amount){
-        this.balance -= amount;
-        const withdrawalTransaction = {
-            date: (new Date()).toLocaleDateString('en-GB'),
-            credit: '',
-            debit: amount,
-            balance: this.balance
-          };
-          this.transaction.push(withdrawalTransaction);
-    
-    }
+  bankBalance () {
+    return this.balance
+  }
 
-    bankBalance(){
-        return this.balance
-    }
+  printStatement () {
+    const statement = ['date || credit || debit || balance']
+    this.transaction.reverse().forEach(transaction => {
+      const date = transaction.date
+      const credit = transaction.credit ? transaction.credit.toFixed(2) : ''
+      const debit = transaction.debit ? transaction.debit.toFixed(2) : ''
+      const balance = transaction.balance.toFixed(2)
+      statement.push(`${date} || ${credit} ||${debit}|| ${balance}`)
+    })
+    return statement.join('\n')
+  }
+};
 
-    printStatement() {
-        let statement = ['date || credit || debit || balance'];
-        this.transaction.reverse().forEach(transaction => {
-            let date = transaction.date;
-            let credit = transaction.credit ? transaction.credit.toFixed(2) : '' ;
-            let debit = transaction.debit ? transaction.debit.toFixed(2) : '' ;
-            let balance = transaction.balance.toFixed(2);
-            statement.push(`${date} || ${credit} ||${debit}|| ${balance}`);
-        });
-       
-        return statement.join('\n');
-    };   
- };
-module.exports = Account;
+module.exports = Account
